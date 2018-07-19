4aecc8c91c3f6a8f43670a1072df416cbcf05b8e
Mukundmurarka <mukundmurarka1996@gmail.com>
2018-07-19 15:09:37 +0530

    forth

--- app/src/main/java/com/mukundmurarka/medicinereminder/Alarm.java
+++ app/src/main/java/com/mukundmurarka/medicinereminder/Alarm.java
@@ -1,21 +1,56 @@
 package com.mukundmurarka.medicinereminder;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class Alarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-       //Toast.makeText(Alarm.this,"Alarm.....",Toast.LENGTH_LONG).show();
 
-        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(10000);
+        sendNotification(context);
+    }
+    private void sendNotification(Context context){
 
+        NotificationCompat.Builder mBuilder = new
+                NotificationCompat.Builder(context, "new_notify")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Medicine Reminder")
+                .setContentText("Pls take your Medicine on Time!!");
+
+        Intent intent = new Intent(context,Main2Activity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        mBuilder.setContentIntent(pendingIntent);
+
+        //    int mNotificationId = 001;
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+
+        //   notificationManager.notify(mNotificationId, mBuilder.build());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("new_notify",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Uri alarmsound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.mkmk);
+        mBuilder.setSound(alarmsound);
+
+        long[] vibrate = {100,200,300,400};
+        mBuilder.setVibrate(vibrate);
+
+        notificationManager.notify(0, mBuilder.build());
     }
 }
